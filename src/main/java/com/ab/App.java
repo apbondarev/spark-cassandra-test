a4c8f7fdd144950206e1e8a516460eb68dbb4c9f
apbondarev <apbondarev@gmail.com>
2018-02-11 22:15:25 +0300

    join

--- src/main/java/com/ab/App.java
+++ src/main/java/com/ab/App.java
@@ -4,8 +4,10 @@ import com.datastax.spark.connector.japi.CassandraJavaUtil;
 import com.datastax.spark.connector.japi.CassandraRow;
 import com.datastax.spark.connector.japi.SparkContextJavaFunctions;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class App {
 
@@ -17,14 +19,31 @@ public class App {
         JavaSparkContext sc = new JavaSparkContext(conf);
         SparkContextJavaFunctions cassandraFunctions = CassandraJavaUtil.javaFunctions(sc);
 
-        JavaRDD<String> cassandraRowsRDD = cassandraFunctions
-                .cassandraTable("my_cassandra_keyspace", "employee")
-                .map(CassandraRow::toString);
+        JavaRDD<CassandraRow> employeeRdd = cassandraFunctions
+                .cassandraTable("my_cassandra_keyspace", "employee");
+        print(employeeRdd);
 
-        String concat = cassandraRowsRDD.reduce((s1, s2) -> s1 + '\n' + s2);
-        System.out.println("Data as CassandraRows: \n" + concat);
+        JavaRDD<CassandraRow> departmentRdd = cassandraFunctions
+                .cassandraTable("my_cassandra_keyspace", "department");
+        print(departmentRdd);
+
+        JavaPairRDD<Integer, Tuple2<Integer, CassandraRow>> departmentIdToEmployeePairs = employeeRdd
+                .mapToPair((CassandraRow row) -> new Tuple2(row.getInt("dep_id"), row));
+        JavaPairRDD<Integer, Tuple2<Integer, CassandraRow>> idToDeparmentPairs = departmentRdd
+                .mapToPair((CassandraRow row) -> new Tuple2(row.getInt("id"), row));
+
+        JavaRDD<Tuple2<Tuple2<Integer, CassandraRow>, Tuple2<Integer, CassandraRow>>> employeeWithDeparment =
+                departmentIdToEmployeePairs.join(idToDeparmentPairs).values();
+        print(employeeWithDeparment);
 
         sc.stop();
+    }
+
+    private static void print(JavaRDD<?> rdd) {
+        String str = rdd
+                .map(Object::toString)
+                .reduce((s1, s2) -> s1 + '\n' + s2);
+        System.out.println(str);
     }
 
 }
